ba6bf4d48167997ad2266d3073c0063f69b261a5
Robert Hogan <rhogan7@gmail.com>
2021-02-22 19:46:42 -0800

    Merged 2nd fetch values needed into 1st object.

---
+++ dist/js/helper.js
@@ -0,0 +1,16 @@
+    <div id="star-wars" class="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
+            <img class="w-full opacity-50" src="https://i.pinimg.com/originals/ab/59/08/ab59083db9af54986221c5c1409e26a9.gif" alt="" />
+            <div class="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xxl font-bold rounded">In Development</div>
+            <div class="info-box text-xs flex p-1 font-semibold text-gray-500 bg-gray-300">
+                <span class="mr-1 p-1 px-2 font-bold" id="height"></span>
+                <span class="mr-1 p-1 px-2 font-bold border-l border-gray-400" id="mass"></span>
+                <span class="mr-1 p-1 px-2 font-bold border-l border-gray-400" id="date-of-birth"></span>
+            </div>
+            <div class="desc p-4 text-gray-800">
+                <a href="https://www.youtube.com/watch?v=dvqT-E74Qlo" target="_new" class="title font-bold block cursor-pointer hover:underline" id="name"></a>
+                <a id="home-world" href="" target="_new" class="badge bg-indigo-500 text-blue-100 rounded px-1 text-xs font-bold cursor-pointer"></a>
+                <span class="description text-sm block py-2 border-gray-400 mb-2">Star Wars API: Pulling character information.  Working on scraping images to use for the charcter profile.</span>
+            </div>
+        </div>
+    </div>
